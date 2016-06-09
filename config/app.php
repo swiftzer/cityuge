4f1bc1239f30840745b26ba3928d7d72f190d944
Eric Li <eric@swiftzer.net>
2016-06-09 14:30:14 +0800

    Add Laravel Debugbar for easy debugging.

--- config/app.php
+++ config/app.php
@@ -201,6 +201,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+
     ],
 
 ];
