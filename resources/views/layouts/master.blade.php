320d0c210f27747a7001b7dd4a7c7cf45d6b30a8
JosephChong <joseph1125@gmail.com>
2016-07-17 13:06:37 +0800

    set up env for front end development set up the navbar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,11 +5,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>CityU GE Guide</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css"
-        integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   <link rel="alternate" href="{{ route('feed') }}" title="CityU GE Guide - Latest reviews" type="application/atom+xml">
 </head>
 <body>
+@include('layouts.nav')
 <div class="container">
   <h1><a href="{{ route('home') }}">CityU GE Guide</a></h1>
   <nav>
@@ -24,5 +24,6 @@
 </div>
 
 @yield('content')
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+                aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="{{ route('home') }}" class="navbar-brand">CityU GE Guide</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Courses <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="{{ route('courses.index') }}">All Courses</a></li>
+                        <li><a href="{{ route('departments.index') }}">Academic units</a></li>
+                        <li><a href="#">Statistics</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Area 1</a></li>
+                        <li><a href="#">Area 2</a></li>
+                        <li><a href="#">Area 3</a></li>
+                        <li><a href="#">University Requirements</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Foundation</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Advanced search</a></li>
+                    </ul>
+                </li>
+                <li><a href="{{ route('reviews.index') }}">Comments</a></li>
+            </ul>
+            
+            <form class="navbar-form navbar-right" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Course code or title">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+    </div>
+</nav>
