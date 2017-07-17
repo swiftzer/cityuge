930c987ee67fb057bfdd0df615bfe5af752b090c
ayame30 <alnaym30@gmail.com>
2017-07-17 23:18:36 +0800

    Update routes and config for i18n

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,28 +10,37 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'before' => 'LaravelLocalizationRedirectFilter',
+    ],
 
-Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
+    function()
+    {
+        Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
 
-// Courses
-Route::get('courses', ['as' => 'courses.index', 'uses' => 'CourseController@index']);
-Route::get('courses/categories/{category}/{semester?}', ['as' => 'courses.category', 'uses' => 'CourseController@category']);
-Route::get('courses/{course}', ['as' => 'courses.show', 'uses' => 'CourseController@show']);
+        // Courses
+        Route::get('courses', ['as' => 'courses.index', 'uses' => 'CourseController@index']);
+        Route::get('courses/categories/{category}/{semester?}', ['as' => 'courses.category', 'uses' => 'CourseController@category']);
+        Route::get('courses/{course}', ['as' => 'courses.show', 'uses' => 'CourseController@show']);
 
-// Search
-Route::get('search', ['as' => 'search.index', 'uses' => 'SearchController@index']);
-Route::get('search/autocomplete', ['as' => 'search.autocomplete', 'uses' => 'SearchController@autocomplete']);
-Route::get('search/results', ['as' => 'search.results', 'uses' => 'SearchController@results']);
+        // Search
+        Route::get('search', ['as' => 'search.index', 'uses' => 'SearchController@index']);
+        Route::get('search/autocomplete', ['as' => 'search.autocomplete', 'uses' => 'SearchController@autocomplete']);
+        Route::get('search/results', ['as' => 'search.results', 'uses' => 'SearchController@results']);
 
-// Departments
-Route::get('departments', ['as' => 'departments.index', 'uses' => 'DepartmentController@index']);
-Route::get('departments/{department}', ['as' => 'departments.courses', 'uses' => 'DepartmentController@courses']);
+        // Departments
+        Route::get('departments', ['as' => 'departments.index', 'uses' => 'DepartmentController@index']);
+        Route::get('departments/{department}', ['as' => 'departments.courses', 'uses' => 'DepartmentController@courses']);
 
-// Reviews
-Route::get('reviews', ['as' => 'reviews.index', 'uses' => 'ReviewController@index']);
-Route::get('courses/{course}/reviews/create', ['as' => 'reviews.create', 'uses' => 'ReviewController@create']);
-Route::post('reviews/store', ['as' => 'reviews.store', 'uses' => 'ReviewController@store']);
-Route::get('reviews/{review}', ['as' => 'reviews.show', 'uses' => 'ReviewController@show']);
+        // Reviews
+        Route::get('reviews', ['as' => 'reviews.index', 'uses' => 'ReviewController@index']);
+        Route::get('courses/{course}/reviews/create', ['as' => 'reviews.create', 'uses' => 'ReviewController@create']);
+        Route::post('reviews/store', ['as' => 'reviews.store', 'uses' => 'ReviewController@store']);
+        Route::get('reviews/{review}', ['as' => 'reviews.show', 'uses' => 'ReviewController@show']);
 
-// Atom feed
-Route::get('feed', ['as' => 'feed', 'uses' => 'ReviewController@recentAtomFeed']);
+        // Atom feed
+        Route::get('feed', ['as' => 'feed', 'uses' => 'ReviewController@recentAtomFeed']);
+    }
+);
